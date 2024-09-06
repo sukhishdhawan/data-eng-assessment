fed272ff2383017d652858d9bf06f17bfb4e815c
Sukhish Dhawan <sukhishdhawan@gmail.com>
2024-09-06 20:57:27 +0530

    delete and update commands

--- dags/utils/gs_to_bigquery.py
+++ dags/utils/gs_to_bigquery.py
@@ -3,10 +3,19 @@ from google.cloud import bigquery
 def load_parquet_to_bigquery(project_id, dataset_id, table_id, exec_date):
     gcs_uri = f'gs://nyc-taxi-dhawan/NYC_TAXI/{table_id}_{exec_date[:7]}.parquet'
     client = bigquery.Client(project=project_id)
+    #Delete Records before inserting
+    delete_query = f"""
+    DELETE FROM `{project_id}.{dataset_id}.{table_id}`
+    WHERE file_month = PARSE_DATE("%Y-%m-%d", "{exec_date[:7]}-01")
+    """
+    delete_job = client.query(delete_query)
+    delete_job.result()
+    print(f"Deleted records for month {exec_date[:7]}")
+    #Load Data
     table_ref = f"{project_id}.{dataset_id}.{table_id}"
     job_config = bigquery.LoadJobConfig(
         source_format=bigquery.SourceFormat.PARQUET,  
-        autodetect=True,  
+        autodetect=True,
         write_disposition=bigquery.WriteDisposition.WRITE_APPEND,  
     )
     load_job = client.load_table_from_uri(
@@ -21,3 +30,12 @@ def load_parquet_to_bigquery(project_id, dataset_id, table_id, exec_date):
         raise Exception(f"Failed to load data: {load_job.error_result}")
     
     print(f"Loaded {load_job.output_rows} rows into {table_ref}.")
+    #Update file_month after insertion
+    update_query = f"""
+    UPDATE `{project_id}.{dataset_id}.{table_id}`
+    SET file_month = PARSE_DATE("%Y-%m-%d", "{exec_date[:7]}-01")
+    WHERE file_month IS NULL
+    """
+    update_job = client.query(update_query)
+    update_job.result()
+    print(f"Updated records for month {exec_date[:7]}")
